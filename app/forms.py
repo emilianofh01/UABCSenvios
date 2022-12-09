72fac487fc2483931e586878c838a945439586e9
Emiliano Fernandez <emilianofh02@gmail.com>
2022-12-09 11:50:02 -0700

    Estilos de login

--- app/forms.py
+++ app/forms.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import User
 
 class UserLoginForm (forms.Form):
     
-    username= forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password= forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username= forms.CharField(label='Nombre de usuario', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password= forms.CharField(label='Contraseña', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class UserRegister (UserCreationForm):
     
-    username= forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email=forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password1= forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2= forms.CharField(label='Password verification', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username= forms.CharField(label='Nombre de usuario', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email=forms.EmailField(label='Correo electronico',widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1= forms.CharField(label='Contraseña', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2= forms.CharField(label='Verificacion de contraseña', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class Meta():
     model=User
