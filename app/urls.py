ce6cd953394e9e1cb8161ca5769dbffa8373ac16
Rafael Castillejos <armandocastillejos086@gmail.com>
2022-12-09 01:59:57 -0700

    Se ha iniciado sesión con éxito

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from .forms import UserLoginForm
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     
@@ -8,4 +9,6 @@ urlpatterns = [
     path('acerca/', views.acerca, name='acerca'),
     path('servicios/', views.servicio, name='servicios'),
     path('contacto/', views.contacto, name='contacto'),
+    path('login/', views.login, name='login_user'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import UserLoginForm 
 # Create your views here.
 def index(request): 
     return render(request, 'app/index.html')
@@ -12,3 +12,10 @@ def acerca(request):
 
 def contacto(request): 
     return render(request, 'app/contacto.html')
+
+def login(request):
+    form= UserLoginForm()
+    ctx={'form':form}
+    return render(request, 'app/login.html',ctx)
+
+    
