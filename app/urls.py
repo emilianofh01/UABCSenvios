32a4eca493cb2fcd96d554cc7170978ba97fc920
Emiliano Fernandez <emilianofh02@gmail.com>
2022-12-10 12:11:17 -0700

    Pequeños cambios

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('login/', views.login_user, name='login_user'),
     path('register/', views.register, name='register_user'),
-    
+    path('panel/', views.panel, name='panel'),
+
 ]
