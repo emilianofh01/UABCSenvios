ea6d6772422d916bf58a85f4444b9d2fd3fed7e9
Christian Duarte <w.christian.duarte@outlook.com>
2022-12-10 19:01:31 -0700

    Filter function js

--- app/static/app/js/app.js
+++ app/static/app/js/app.js
@@ -19,5 +19,31 @@ function init (){
 
     axios.defaults.withCredentials = true;
     axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
+
+    window.toQueryParams = (obj) => {
+        return Object.keys(obj).length > 0 ? '?' + Object.keys(obj).map(key => `${key}=${encodeURI(obj[key])}`).join('&') : '';
+    };
+
+    remapKeys = window.remapKeys = (obj, keyMap, only = false) =>{
+        const r = only ? {} : obj;
+        Object.entries(keyMap).forEach(e => {
+            if(obj[e[0]]){
+                r[e[1]] = obj[e[0]];
+                delete obj[e[0]];
+            }
+        })
+        return r;
+    }
+
+    window.drones = {
+        getAll(filter = {}){
+            filter = remapKeys(filter, {nombre: "nombre__icontains"}, true);
+            return axios.get("dron/" + window.toQueryParams(filter))
+        }
+    };
+
+
+
 }
+
 window.addEventListener('load', init);
