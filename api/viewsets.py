31c056f01642f330f661c41bd63719843be227da
Christian Duarte <w.christian.duarte@outlook.com>
2022-12-10 17:57:25 -0700

    Fixed bug on models

--- api/viewsets.py
+++ api/viewsets.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import viewsets
-from api.models import Dron
+from app.models import Dron
 from api.filters import *
 
 
