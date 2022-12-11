2909a262b278bde347f0409af8553faf6b41e238
Emiliano Fernandez <emilianofh02@gmail.com>
2022-12-10 19:02:08 -0700

    Maquetado drones

--- app/static/app/js/app.js
+++ app/static/app/js/app.js
@@ -20,4 +20,5 @@ function init (){
     axios.defaults.withCredentials = true;
     axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
 }
+
 window.addEventListener('load', init);
---
+++ app/static/app/js/panel.js
@@ -0,0 +1,10 @@
+let searchBar = document.querySelector(".searchDrone");
+let drone_list = document.querySelector(".drone_list");
+
+searchBar.addEventListener('input', () => {
+    
+
+    drone_list.innerHTML = "";
+
+    
+})
