86186d13cd9fda11f6bcf4764a1744b8dddec343
Emiliano Fernandez <emilianofh02@gmail.com>
2022-12-10 18:22:02 -0700

    Pequeños cambios

--- app/static/app/js/app.js
+++ app/static/app/js/app.js
@@ -18,6 +18,6 @@ function init (){
     });
 
     axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = "http://localhost:8000/api/";
+    axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
 }
 window.addEventListener('load', init);
